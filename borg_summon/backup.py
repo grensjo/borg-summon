ce310a39c352d222dca4940672de9c39e862f1f3
Anton Grensjö <anton@grensjo.se>
2017-03-14 01:03:03 +0100

    Redesign the configuration format and semantics

--- borg_summon/backup.py
+++ borg_summon/backup.py
@@ -4,29 +4,31 @@ import sys
 import sh
 import os.path
 from datetime import datetime
+from collections import ChainMap
 from . import util
 
 
 @click.command()
 @click.argument('sources', nargs=-1)
 @click.pass_obj
-def main(global_config, sources):
+def main(config, sources):
+    command_config = ChainMap(config['backup']['create'], config) #TODO defaults
+
     if len(sources) == 0:
-        sources = global_config['backup']['sources'].keys()
+        sources = config['backup']['sources'].keys()
 
     for name in sources:
-        source_config = util.get_recursive_dict(global_config, ['backup', 'sources', name]) 
+        source_config = command_config.new_child(config['backup']['sources'][name])
         
         if 'remote_list' in source_config:
             remote_list = source_config['remote_list']
         else:
-            remote_list = global_config['remotes'].keys()
+            remote_list = config['remotes'].keys()
 
         for remote in remote_list:
-            remote_config = util.get_prioritized_recursive_dict(global_config,
-                    ['backup', 'sources', name], ['remotes', remote])
+            remote_config = source_config.new_child(config['remotes'][remote])
 
-            cmd = ['create']
+            cmd = []
 
             if 'log_level' in remote_config:
                 level = remote_config['log_level']
@@ -89,6 +91,6 @@ def main(global_config, sources):
             print("Backing up", name, "to", remote)
             if remote_config.get('sudo', False):
                 with sh.contrib.sudo:
-                    sh.borg(*cmd, _fg=True, _env=env)
+                    sh.borg.create(*cmd, _fg=True, _env=env)
             else:
-                sh.borg(*cmd, _fg=True, _env=env)
+                sh.borg.create(*cmd, _fg=True, _env=env)
